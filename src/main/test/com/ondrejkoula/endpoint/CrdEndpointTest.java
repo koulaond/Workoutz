54d8f66ebae0c3af4214de569da2440cb7d618a7
Koula <ondrej.koula@memsource.com>
2022-06-27 23:27:13 +0200

    Composite endpoints - tests

--- src/main/test/com/ondrejkoula/endpoint/CrdEndpointTest.java
+++ src/main/test/com/ondrejkoula/endpoint/CrdEndpointTest.java
@@ -4,15 +4,19 @@ import com.ondrejkoula.domain.exercise.ExerciseType;
 import com.ondrejkoula.dto.exercise.ExerciseTypeDTO;
 import com.ondrejkoula.endpoint.exercise.ExerciseTypeEndpoint;
 import com.ondrejkoula.service.exercise.ExerciseTypeService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static com.ondrejkoula.domain.exercise.ExerciseType.builder;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class CrdEndpointTest {
@@ -28,23 +32,38 @@ class CrdEndpointTest {
     }
 
     @Test
-    void get() {
-        Mockito.doReturn(ExerciseType.builder().note("exerciseType").build())
+    void testGet() {
+        Mockito.doReturn(buildTestExerciseType())
                 .when(exerciseTypeService).findById(1L);
 
         ExerciseTypeDTO exerciseTypeDTO = endpoint.get(1L);
-        Assertions.assertThat(exerciseTypeDTO.getNote()).isEqualTo("exerciseType");
+
+        assertThat(exerciseTypeDTO.getNote()).isEqualTo("exerciseType");
     }
 
     @Test
-    void list() {
+    void testList() {
+        Mockito.doReturn(singletonList(buildTestExerciseType()))
+                .when(exerciseTypeService).findAll();
+
+        List<ExerciseTypeDTO> list = endpoint.list();
+
+        assertThat(list).hasSize(1)
+                .satisfies(exerciseTypeDTO -> assertThat(exerciseTypeDTO.get(0).getNote()).isEqualTo("exerciseType"));
     }
 
-    @Test
-    void create() {
-    }
 
     @Test
-    void delete() {
+    void testCreate() {
+        ExerciseType expected = buildTestExerciseType();
+        Mockito.doReturn(expected).when(exerciseTypeService).create(ArgumentMatchers.any());
+
+        ExerciseTypeDTO returned = endpoint.create(ExerciseTypeDTO.builder().note("exerciseType").build());
+
+        assertThat(returned.getNote()).isEqualTo(expected.getNote());
     }
+    private ExerciseType buildTestExerciseType() {
+        return builder().note("exerciseType").build();
+    }
+
 }
